e3d24aa3d6406653475a11da09c48b5877524bf5
Pedro <ph-reis1993@live.com>
2024-05-15 15:02:14 -0300

    Fix authentication at Sign in page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:confirmable,
-         :lockable, :timeoutable, :trackable,
+         :recoverable, :rememberable, :validatable,
          authentication_keys: [:username]
+
+  validates :username, presence: true, uniqueness: true
+  validates :fullname, :role, presence: true
+
+  enum role: {admin: 'admin', user: 'user'}
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
 end
