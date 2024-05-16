3dc08fb8d0bad169bb51b0d1755117ebbf3dcc2e
Pedro <ph-reis1993@live.com>
2024-05-15 23:54:58 -0300

    Add stylization features with  bootstrap

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,5 +1,5 @@
 class AlbumsController < ApplicationController
-  before_action :set_album, only: %i[ show edit update destroy ]
+  before_action :set_album, only: %i[show edit update destroy]
 
   # GET /albums or /albums.json
   def index
@@ -7,16 +7,17 @@ class AlbumsController < ApplicationController
   end
 
   # GET /albums/1 or /albums/1.json
-  def show
-  end
+  def show; end
 
   # GET /albums/new
   def new
     @album = Album.new
+    @artists = Artist.all
   end
 
   # GET /albums/1/edit
   def edit
+    @artists = Artist.all
   end
 
   # POST /albums or /albums.json
@@ -25,7 +26,7 @@ class AlbumsController < ApplicationController
 
     respond_to do |format|
       if @album.save
-        format.html { redirect_to album_url(@album), notice: "Album was successfully created." }
+        format.html { redirect_to album_url(@album), notice: 'Album was successfully created.' }
         format.json { render :show, status: :created, location: @album }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +39,7 @@ class AlbumsController < ApplicationController
   def update
     respond_to do |format|
       if @album.update(album_params)
-        format.html { redirect_to album_url(@album), notice: "Album was successfully updated." }
+        format.html { redirect_to album_url(@album), notice: 'Album was successfully updated.' }
         format.json { render :show, status: :ok, location: @album }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,22 +50,29 @@ class AlbumsController < ApplicationController
 
   # DELETE /albums/1 or /albums/1.json
   def destroy
-    @album.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to albums_url, notice: "Album was successfully destroyed." }
-      format.json { head :no_content }
+    if current_user.admin?
+      @album.destroy!
+      respond_to do |format|
+        format.html { redirect_to albums_url, notice: 'Album was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to albums_url, notice: "You can't delete this album. Your role is #{current_user.role}" }
+        format.json { head :no_content }
+      end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_album
-      @album = Album.includes(:artist).find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def album_params
-      params.require(:album).permit(:name, :year)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_album
+    @album = Album.includes(:artist).find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def album_params
+    params.require(:album).permit(:name, :year, :artist_id)
+  end
 end
